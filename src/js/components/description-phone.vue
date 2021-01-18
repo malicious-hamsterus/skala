0c549cc28c2ba3e1a264c4f724d034be9f175285
roman shesternin <malicious.hamsterus@gmail.com>
2021-01-18 10:35:24 +0600

    Поправлен блок с телефонами

--- src/js/components/description-phone.vue
+++ src/js/components/description-phone.vue
@@ -8,9 +8,9 @@
               <a href="tel:+77773160001">+ 7 777 316 0001</a>
               <div class="social">
                 <!-- <a target="_blank" href="https://vk.com/public189782356" class="vk"></a> -->
-                <a target="_blank" href="https://www.instagram.com/skala_boulder/" class="inst">@skalaboulder</a>
-                <a target="_blank" href="https://www.instagram.com/boulder_monkey/" class="inst">@bouldermonkey</a>
-                <a target="_blank" href="https://www.facebook.com/skalaboulder" class="fb">skalaboulder</a>
+                <a target="_blank" href="https://www.instagram.com/skala_boulder/" class="inst"><span class="social-deskript"> @skalaboulder</span></a>
+                <a target="_blank" href="https://www.instagram.com/boulder_monkey/" class="inst"><span class="social-deskript">@bouldermonkey</span></a>
+                <a target="_blank" href="https://www.facebook.com/skalaboulder" class="fb"><span class="social-deskript">skalaboulder</span></a>
               </div>
               <p>Адрес: Абая, 44, Дворец Спорта им. Балуана Шолака, вход со стороны улицы Байтурсынова</p>
             </div>
@@ -21,20 +21,19 @@
         .social {
     display: flex;
     a {
-      margin-top: 7px;
+      background-position: 0 6px;
+      height: 40px;
+      display: flex;
+      align-items: center;
       margin-right: 13px;
       display: block;
-      width: 30px;
-      height: 30px;
-      border-radius: 50%;
+      background-repeat: no-repeat;
+      padding-left: 35px;
       opacity: 0.8;
       &:hover {
         background-color: #ffffff;
         opacity: 1;
       }
-    }
-    & a:first-child{
-      margin-left: 30px;
     }
     .vk {
       background-image: url(../../assets/img/vk.svg);
@@ -52,9 +51,13 @@
       padding: 10px;
     }
     .social {
+      margin: 10px 0;
       display: flex;
       flex-direction: column;
       a {
+        .social-deskript {
+          display: inline;
+        }
         padding-left: 40px;
         border-radius: 0;
         background-repeat: no-repeat;
